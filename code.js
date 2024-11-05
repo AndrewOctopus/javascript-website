4736344819fc4ffef22df6cf0037d1644cfdb8bc
AndrewOctopus <andrewtbatty@gmail.com>
2024-11-05 12:51:01 -0500

    Does this work?

--- code.js
+++ code.js
@@ -1,4 +1,5 @@
 function onClick(inputName) {
-    document.getElementById("output").innerHTML = "Welcome " + inputName.repeat(99).toUpperCase() + ' How are you doing?'
-    alert("Well, Well, Well. What do you know? Its " + inputName + ' What do you know?')
+    const repeatuppercase = inputName.repeat(9999).toUpperCase()
+    document.getElementById("output").innerHTML = "Welcome " + inputName.split('') + ' How are you doing?'
+    alert("Well, Well, Well. What do you know? Its " + inputName + repeatuppercase + repeatuppercase + ' What do you know?')
 }
