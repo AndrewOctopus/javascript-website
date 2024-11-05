4d0c410bd554ee68e63b18ffbccc7c1e67efd540
AndrewOctopus <andrewtbatty@gmail.com>
2024-11-05 12:39:15 -0500

    Safety for tinkering and adding whatever i want.

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
 function onClick(inputName) {
-    document.getElementById("output").innerHTML = "Welcome" + inputName + 'How are you doing?'
+    document.getElementById("output").innerHTML = "Welcome " + inputName.repeat(99).toUpperCase() + ' How are you doing?'
     alert("Well, Well, Well. What do you know? Its " + inputName + ' What do you know?')
 }
