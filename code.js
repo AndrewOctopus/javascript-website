a7e3ee0433243759fa3f23e8fa77b71bc42187a4
AndrewOctopus <andrewtbatty@gmail.com>
2024-11-11 13:00:06 -0500

    Whole bunch of stuff

--- code.js
+++ code.js
@@ -1,5 +1,11 @@
 function onClick(inputName) {
+    const sum = add(2.145265)
+    const war = "I am constantly at war with the bunnies!" 
     const repeatuppercase = inputName.repeat(9999).toUpperCase()
-    document.getElementById("output").innerHTML = "Welcome " + inputName.split('') + ' How are you doing?'
+    document.getElementById("output").innerHTML = "The answer is: " + sum + "........... " + inputName.split('').map(item => {return item + "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" }) + ' How are you doing?'
     alert("Well, Well, Well. What do you know? Its " + inputName + repeatuppercase + repeatuppercase + ' What do you know?')
+}
+
+function add(a) {
+    return a + 1
 }
