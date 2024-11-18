9d91f7473e25bdf3ca599513e14c36410c612382
AndrewOctopus <andrewtbatty@gmail.com>
2024-11-18 12:32:59 -0500

    Whatever we changed last time

--- code.js
+++ code.js
@@ -1,11 +1,24 @@
 function onClick(inputName) {
-    const sum = add(2.145265)
-    const war = "I am constantly at war with the bunnies!" 
-    const repeatuppercase = inputName.repeat(9999).toUpperCase()
-    document.getElementById("output").innerHTML = "The answer is: " + sum + "........... " + inputName.split('').map(item => {return item + "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" }) + ' How are you doing?'
-    alert("Well, Well, Well. What do you know? Its " + inputName + repeatuppercase + repeatuppercase + ' What do you know?')
+  const sum = add(2.01, 1.13);
+  const war = "I am constantly at war with the bunnies!";
+  const repeatuppercase = inputName.repeat(9999).toUpperCase();
+  const name = "john";
+  let age = 11;
+  age = 1213141516171819202122232425262728293031323334;
+  document.getElementById("output").innerHTML =
+    "The answer is: " + sum + "........... ";
+  Add("I dont now wat im doin");
+  age = Add()
 }
 
-function add(a) {
-    return a + 1
+function Add() {
+    return 1 + 1;
+}
+
+function Add(parameter) {
+  alert("This, answer, is, not - correct");
+}
+
+function add(a, bunnies) {
+  return a + bunnies;
 }
