9a6a91b934aed04094e87a5f425b08d7e59cb9ca
AndrewOctopus <andrewtbatty@gmail.com>
2024-12-10 13:32:43 -0500

    RandomBoredomCommit

--- code.js
+++ code.js
@@ -1,18 +1,30 @@
 function onClick(inputName) {
-  const sum = add(2.01, 1.13);
+  $.get(
+    "https://pokeapi.co/api/v2/pokemon/" + inputName,
+    function (data, status) {
+      document.getElementById("output").innerHTML = JSON.stringify(data);
+      console.log(data);
+      console.log(status);
+      console.log(data.sprites.other["official-artwork"].front_default);
+    }
+  );
+}
+
+function oldOnclick(inputName) {
+  const sum = add(0.04, 3.1);
   const war = "I am constantly at war with the bunnies!";
   const repeatuppercase = inputName.repeat(9999).toUpperCase();
   const name = "john";
   let age = 11;
-  age = 1213141516171819202122232425262728293031323334;
+  age = 13141516171819202122232425262728293031323334;
   document.getElementById("output").innerHTML =
     "The answer is: " + sum + "........... ";
   Add("I dont now wat im doin");
-  age = Add()
+  age = Add();
 }
 
 function Add() {
-    return 1 + 1;
+  return 1 + 1;
 }
 
 function Add(parameter) {
